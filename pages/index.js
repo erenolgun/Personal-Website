d76ed5378f552a1e3422116b98a107879ccc602a
Mehmet Eren Olgun <mehmeterenolgun@gmail.com>
2021-07-05 22:00:24 +0300

    Made index.js responsive

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,11 @@ const Home = () => {
     <Layout>
       <div className='p-12 md:p-10 lg:p-12 xl:container xl:mx-auto'>
         <header className='mb-11'>
-          <h1 className='font-serif text-7xl font-bold'>
+          <h1 className='text-6xl sm:text-7xl font-semibold font-serif'>
             Frontend web developer.
           </h1>
         </header>
-        <p className='text-2xl mb-11'>
+        <p className='text-xl mb-8 sm:text-2xl sm:mb-11'>
           I'm a third year computer engineering student at Ankara Yildirim
           Beyazit University, Turkey. I’m interested in frontend web development
           with over 2 years. Currently working as an Frontend Web Developer
@@ -21,16 +21,16 @@ const Home = () => {
           .
         </p>
         <div>
-          <a href='#' className='text-light text-2xl mr-8'>
+          <a href='#' className='text-light text-lg mr-3 sm:text-2xl sm:mr-8'>
             LinkedIn
           </a>
-          <a href='#' className='text-light text-2xl mr-8'>
+          <a href='#' className='text-light text-lg mr-3 sm:text-2xl sm:mr-8'>
             GitHub
           </a>
-          <a href='#' className='text-light text-2xl mr-8'>
+          <a href='#' className='text-light text-lg mr-3 sm:text-2xl sm:mr-8'>
             Behance
           </a>
-          <a href='#' className='text-light text-2xl'>
+          <a href='#' className='text-light text-lg sm:text-2xl'>
             Instagram
           </a>
         </div>
